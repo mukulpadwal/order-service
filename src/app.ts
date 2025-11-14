9f5b1240b30874000e705defad16fddc12d7068a
mukulpadwal <mukulpadwal.me@gmail.com>
2025-11-14 14:16:52 +0530

    add coupon router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import customerRouter from "./customer/customer.route.js";
+import couponRouter from "./coupon/coupon.route.js";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/customer", customerRouter);
+app.use("/api/v1/coupon", couponRouter);
 
 export default app;
---
+++ src/coupon/coupon.route.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const couponRouter = Router();
+
+export default couponRouter;
